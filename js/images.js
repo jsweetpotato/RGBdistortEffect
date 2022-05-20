fe17e384463b21f8a2d5c2d673a451f28688039f
김지수 <jsweetpotato37@gmail.com>
2022-05-20 12:38:36 +0900

    Image data export

---
+++ js/images.js
@@ -0,0 +1,13 @@
+import image1 from "../images/1.jpg";
+import image2 from "../images/2.jpg";
+import image3 from "../images/3.jpg";
+import image4 from "../images/4.jpg";
+
+const images = {
+  image1,
+  image2,
+  image3,
+  image4,
+};
+
+export { images };
